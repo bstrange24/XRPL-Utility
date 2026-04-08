26e71842573a3eba781369c656c23b32058d16d2
bstra <bstrange24@yahoo.com>
2026-04-08 09:17:35 -0400

    Reverted PR #13. It broke the entire browser

--- xrpl-app/src/app/components/trustlines/trustlines.component.ts
+++ xrpl-app/src/app/components/trustlines/trustlines.component.ts
@@ -338,7 +338,6 @@ export class TrustlinesComponent extends WalletDestinationBase implements OnInit
 
      protected refreshAccountObject(env: any): void {
           this.trustlineStoreService.setField('existingIOUs', this.trustlineCurrencyService.getExistingIOUs(env.accountObjects, env.wallet.classicAddress));
-          this.storageService.setAccountObjects(env.wallet.classicAddress, env.accountObjects);
      }
 
      /** Stale-while-revalidate: populate existingIOUs from cached account objects instantly. */
